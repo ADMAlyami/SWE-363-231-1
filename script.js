7cff305480f91af7b72efa61bd17c99dea85e38f
Ali The ali <alitheali@Alis-MacBook-Pro.local>
2023-10-27 17:40:22 +0300

    Javascript3_Exercise3

--- script.js
+++ script.js
@@ -20,6 +20,46 @@ document.addEventListener("DOMContentLoaded", function() {
       li.appendChild(a);
       navbar.appendChild(li);
   });
+
+  // javaScript 3 Exercise 3
+  let timeoutId;
+  function resetScreensaverTimer() {
+    clearTimeout(timeoutId);
+    document.getElementById("screensaver").style.display = "none";
+    timeoutId = setTimeout(showScreensaver, 60000);  // 1 minute
+  }
+
+  function showScreensaver() {
+    const screensaverDiv = document.getElementById("screensaver");
+    screensaverDiv.style.display = "block";
+
+    // Three.js code for screensaver
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    screensaverDiv.appendChild(renderer.domElement);
+
+    const geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+    const torusKnot = new THREE.Mesh(geometry, material);
+    scene.add(torusKnot);
+
+    camera.position.z = 30;
+
+    function animate() {
+      requestAnimationFrame(animate);
+      torusKnot.rotation.x += 0.01;
+      torusKnot.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    }
+    animate();
+  }
+
+  document.body.addEventListener("mousemove", resetScreensaverTimer);
+  document.body.addEventListener("keypress", resetScreensaverTimer);
+  resetScreensaverTimer();  // Init screensaver timer
+
 });
 
 // exercise 2 and i will change the navbar to not delay and transition
