3e27f4174405152f6b7e7a48fd80084a87042c21
Ali The ali <alitheali@Alis-MacBook-Pro.local>
2023-11-26 23:55:14 +0300

    BE3_Exercise 3

--- BE_Exercises/app.js
+++ BE_Exercises/app.js
@@ -8,44 +8,14 @@ import { fileURLToPath } from 'url';
 const app = express();
 const port = 3000;
 
-// Define __dirname for ES6 module
-const __dirname = '/Users/alitheali/Desktop/SWE-363/SWE-363-231-1';
-
-// serving all the pages
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/personalBlog.html');
-});
-
-app.get('/game', (req, res) => {
-  res.sendFile(__dirname + '/game.html');
-});
-
-app.get('/details', (req, res) => {
-  res.sendFile(__dirname + '/detailPage.html');
-});
-
-app.get('/arabic', (req, res) => {
-  res.sendFile(__dirname + '/arabicVersion.html');
-});
-
-app.get('/hireMe', (req, res) => {
-  res.sendFile(__dirname + '/hireMe.html');
-});
-
-app.get('/contactUs', (req, res) => {
-  res.sendFile(__dirname + '/contactUs.html');
-});
-
-app.get('/contactUs/thankYou', (req, res) => {
-  res.sendFile(__dirname + '/confirmationPage.html');
-});
-
-app.get('/dog', (req, res) => {
-  res.sendFile(__dirname + '/dogImages.html');
-});
 
 
+import routes from './routers.js';
+
+
+app.use(express.static('/Users/alitheali/Desktop/SWE-363/SWE-363-231-1'));
+app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
